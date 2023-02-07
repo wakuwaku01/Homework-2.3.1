b5e73c842934b09985da3d708b6de7fdbb32f1e7
wakuwaku01 <wakuwaku.post.mail@gmail.com>
2023-02-07 19:29:23 +0300

    corrections

--- Homework-2.3.1/Homework-2.3.1.cpp
+++ Homework-2.3.1/Homework-2.3.1.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
 class calculator {
-public:
+private:
     double num1;
     double num2;
 
+public:
     double add() { return num1 + num2; };
     double multiply() { return num1 * num2; };
     double subtract_1_2() { return num1 - num2; };
@@ -16,45 +17,54 @@ public:
     bool set_num2(double num2);
 };
 
-bool calculator::set_num1(double num1) {
-    if (this->num1 != 0)
+bool calculator::set_num1(double num1_) {
+    if (num1_ != 0) {
+        num1 = num1_;
         return true;
-    else
+    }
+    else {
         return false;
+    }
 }
 
-bool calculator::set_num2(double num2) {
-    if (this->num2 != 0)
+bool calculator::set_num2(double num2_) {
+    if (num2_ != 0) {
+        num2 = num2_;
         return true;
-    else
+    }
+    else {
         return false;
-}
-
-void zero_num(calculator& p) {
-    if (p.num1 == false || p.num2 == false) {
-        std::cout << "Неправильный ввод" << std::endl;
-        if (p.num1 == false) {
-            std::cout << "Введите num1: ";
-            std::cin >> p.num1;
-        }
-        else {
-            std::cout << "Введите num2: ";
-            std::cin >> p.num2;
-        }
     }
 }
 
 int main() {
     setlocale(LC_ALL, "Russian");
     calculator num;
+    double num1;
+    double num2;
 
-    while (true) {
-        std::cout << "Введите num1: ";
-        std::cin >> num.num1;
-        zero_num(num);
-        std::cout << "Введите num2: ";
-        std::cin >> num.num2;
-        zero_num(num);
+    for (;;) {
+        while (true) {
+            std::cout << "Введите num1: ";
+            std::cin >> num1;
+            if (num.set_num1(num1)) {
+                break;
+            }
+            else {
+                std::cout << "Неправильный ввод." << std::endl;
+            }
+        }
+        while (true) {
+            std::cout << "Введите num2: ";
+            std::cin >> num2;
+            if (num.set_num2(num2)) {
+                break;
+            }
+            else {
+                std::cout << "Неправильный ввод." << std::endl;
+            }
+        }
+
         std::cout << "num1 + num2 = " << num.add() << std::endl;
         std::cout << "num1 - num2 = " << num.subtract_1_2() << std::endl;
         std::cout << "num2 - num1 = " << num.subtract_2_1() << std::endl;
